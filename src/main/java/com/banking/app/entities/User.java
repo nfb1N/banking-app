d27c6c5af5f3c348eca378537609a7e385adfffa
Mykyta Nikolaichuk <nikolas20052016@gmail.com>
2025-01-28 20:46:22 +0200

    add User entity

---
+++ src/main/java/com/banking/app/entities/User.java
@@ -0,0 +1,39 @@
+package com.banking.app.entities;
+
+
+import com.banking.app.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String fullName;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<BankAccount> accounts;
+}
