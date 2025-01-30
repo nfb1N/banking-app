70907addee8837c009c75b7ef0fe711dc6cb1daf
Mykyta Nikolaichuk <nikolas20052016@gmail.com>
2025-01-30 04:21:45 +0200

    create a user repository to interact with bd

---
+++ src/main/java/com/banking/app/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.banking.app.repositories;
+
+import com.banking.app.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByEmail(String email);
+}
